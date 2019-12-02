61c58307886032ba0a5abfcb7978748d84e1106f
Dongnguyen <33015582+dongnguyen248@users.noreply.github.com>
2019-12-02 14:25:54 +0700

    Add files via upload

--- bai7Accordion Menu/script.js
+++ bai7Accordion Menu/script.js
@@ -1,18 +1,23 @@
-var accordion = document.getElementsByClassName('submenu');
+var accordion = document.getElementsByClassName('accordion');
 for (var i = 0; i < accordion.length; i++) {
     createMenu(accordion[i]);
+
 }
 
 function createMenu(element) {
     var tbn = element.childNodes[0];
+    console.log(tbn);
     var subMenu = element.childNodes[2];
+
     tbn.addEventListener('click', function(event) {
         event.preventDefault();
-        if (subMenu.style.display == 'none' || subMenu.style.display == '') {
-            subMenu.style.display = 'block';
-        } else { subMenu.style.display = 'none' }
-    })
-    tbn.addEventListener('blur', function(event) {
-        subMenu.style.display = 'none';
-    })
+        if (subMenu.style.maxHeight == '0px' || subMenu.style.maxHeight == '') {
+            subMenu.style.maxHeight = '10em';
+        } else { subMenu.style.maxHeight = '0px' }
+
+
+    });
+    tbn.addEventListener('blur', () => { subMenu.style.maxHeight = '0px'; });
+
+    // console.log(tbn.addEventListener)
 }
